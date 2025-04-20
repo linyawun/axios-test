e9f1b25dbe608add2979dc0ff6bb5b99c379e7e8
linyawun <linyawun031@gmail.com>
2025-04-20 15:12:55 +0800

    refactor: update variable declarations in fetch-demo.js to use const for better code clarity

--- src/2-encode/fetch-demo.js
+++ src/2-encode/fetch-demo.js
@@ -20,12 +20,12 @@ export async function fetchManualEncode() {
     console.log('📢 [Fetch] encode feature - ManualEncode:');
     const search = 'hello world!';
     const symbol = '&$';
-    let res = await fetch(
+    const res = await fetch(
       `${API_URL}/url-encoded?search=${encodeURIComponent(
         search
       )}&symbol=${encodeURIComponent(symbol)}`
     );
-    let data = await res.json();
+    const data = await res.json();
     console.log(data);
   } catch (error) {
     console.error('Fetch error:', error);
